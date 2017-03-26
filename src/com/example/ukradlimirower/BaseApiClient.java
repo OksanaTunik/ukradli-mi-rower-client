95e6d6bcb259f3ad358509ae726675cb182300f0
Artem Shoobovych <shybovycha@gmail.com>
2017-03-26 12:41:46 +0200

    Updating

--- src/com/example/ukradlimirower/BaseApiClient.java
+++ src/com/example/ukradlimirower/BaseApiClient.java
@@ -1,9 +1,9 @@
 package com.example.ukradlimirower;
 
 public class BaseApiClient {
-    protected static String publicUrl = "http://ukradli-mi-rower.eu-gb.mybluemix.net";
+    //protected static String publicUrl = "http://ukradli-mi-rower.eu-gb.mybluemix.net";
 
-    // public final static String publicUrl = "http://192.168.56.1:3000";
+    public final static String publicUrl = "http://192.168.56.1:3000";
     public final static String apiUrl = publicUrl + "/api";
 
     public static String getUrl(String postfix) {
